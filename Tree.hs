3dcd6c8c1b74f0e235aa4fa9d7064305ae258bc7
Jamey Sharp <jamey@minilop.net>
2009-06-12 15:02:53 -0700

    First cut at conversion from GitTree back to AbstractTree.
    Assumes that filenames are still in sorted order after unescaping, which
    isn't true when characters less than '=' are in the filename.

--- Tree.hs
+++ Tree.hs
@@ -2,29 +2,30 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 module Tree where
 
 import Control.Monad
 import Data.Bits
 import Data.Convertible.Base
-import Data.Convertible.Utils
+import Data.Function
+import Data.List
+import Data.Ord
 import Data.Typeable
 import Data.Word
 import Numeric
 
 data Tree meta blob = Tree [(String, meta, Tree meta blob)] | Blob blob
-    deriving Show
+    deriving (Show, Typeable)
 
 newtype AbstractTree blob = AbstractTree (Tree [(String, blob)] blob)
     deriving Show
 
 newtype GitMode = GitMode Word
+    deriving Typeable
 
 instance Show GitMode where
     showsPrec _ (GitMode mode) = showOct mode
-
-instance Typeable GitMode where
-    typeOf _ = mkTypeName "GitMode"
 
 instance Convertible [Char] GitMode where
     -- FIXME: handle irregular files
@@ -41,12 +42,21 @@ escapeEquals = fst . foldr doEscape ([], True) where
     doEscape '=' (rest, True) = ('=' : '=' : rest, True)
     doEscape c (rest, _) = (c : rest, False)
 
+unescapeEquals :: String -> (String, Bool)
+unescapeEquals = fst . foldr doUnescape (([], False), True) where
+    doUnescape '=' ((rest, True), True) = (('=' : rest, False), True)
+    doUnescape '=' ((rest, False), True) = ((rest, True), True)
+    doUnescape c ((rest, meta), _) = ((c : rest, meta), False)
+
 blobify :: String -> a -> (String, GitMode, Tree GitMode a)
 blobify name content = (name, GitMode 0o100644, Blob content)
 
+gitSort :: [(String, meta, Tree meta blob)] -> [(String, meta, Tree meta blob)]
+gitSort = sortBy $ comparing $ \ (name, _, _) -> name
+
 instance Convertible blob GitMode => Convertible (AbstractTree blob) (GitTree blob) where
     safeConvert (AbstractTree (Blob blob)) = return $ GitTree $ Blob blob
-    safeConvert (AbstractTree (Tree tree)) = liftM (GitTree . Tree) $ foldM splititem [] tree where
+    safeConvert (AbstractTree (Tree tree)) = liftM (GitTree . Tree . gitSort) $ foldM splititem [] tree where
 
         splititem tree' (name, meta, item) = do
             GitTree item' <- safeConvert (AbstractTree item)
@@ -58,3 +68,21 @@ instance Convertible blob GitMode => Convertible (AbstractTree blob) (GitTree bl
 
         toGitMode _ (Tree _) = GitMode 0o040000
         toGitMode meta _ = head $ [ mode | ("mode", safeConvert -> Right mode) <- meta ] ++ [ GitMode 0o100644 ]
+
+instance (Typeable blob, Show blob) => Convertible (GitTree blob) (AbstractTree blob) where
+    safeConvert (GitTree (Blob blob)) = return $ AbstractTree $ Blob blob
+    safeConvert (GitTree (Tree tree)) = liftM (AbstractTree . Tree) $ uncurry (merge `on` map snd) $ partition fst unescaped where
+
+        unescaped = [ (meta, (name, mode, item)) | (unescapeEquals -> (name, meta), mode, item) <- tree ]
+
+        -- FIXME: these lists are not always in the same order after unescaping
+        merge ((name, _, Tree meta) : xs) ((name', _, item) : ys) | name == name' = do
+            meta' <- mapM extractmeta meta
+            AbstractTree item' <- safeConvert (GitTree item)
+            rest <- merge xs ys
+            return $ (name, meta', item') : rest
+        merge [] [] = return []
+        merge _ _ = convError "metadata match failed" tree
+
+        extractmeta (name, _, Blob content) = return (name, content)
+        extractmeta blob = convError "bad metadata blob" blob
