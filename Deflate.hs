384bcac6e14d948a5f5d03f8fa350c6d5c2a11d8
Jamey Sharp <jamey@minilop.net>
2009-06-25 12:04:23 -0700

    Make SlidingWindow a Monoid instead of a Monad.
    Just as efficient, and doesn't pretend that (>>=) can do anything
    sensible.

--- Deflate.hs
+++ Deflate.hs
@@ -11,6 +11,7 @@ import qualified Data.Foldable as Foldable
 import Data.List
 import qualified Data.IntMap as IntMap
 import Data.Maybe
+import Data.Monoid
 import Data.Ord
 import qualified Data.Sequence as Seq
 import Data.Word
@@ -147,37 +148,36 @@ parseDeflateBlocks = do
         then return [block]
         else liftM (block :) parseDeflateBlocks
 
-newtype SlidingWindow a = SlidingWindow ((RingBuffer -> L.ByteString) -> RingBuffer -> L.ByteString)
+newtype SlidingWindow = SlidingWindow ((RingBuffer -> L.ByteString) -> RingBuffer -> L.ByteString)
 data RingBuffer = RingBuffer (ForeignPtr Word8) {-# UNPACK #-} !Int
-instance Monad SlidingWindow where
-    (SlidingWindow f) >> (SlidingWindow g) = SlidingWindow (f . g)
-    m >>= f = m >> f undefined
-    return _ = SlidingWindow id
+instance Monoid SlidingWindow where
+    (SlidingWindow f) `mappend` (SlidingWindow g) = SlidingWindow (f . g)
+    mempty = SlidingWindow id
 
-write :: Word8 -> SlidingWindow ()
+write :: Word8 -> SlidingWindow
 write w = SlidingWindow $ \ k (RingBuffer fp idx) -> inlinePerformIO $ do
     withForeignPtr fp $ \ p -> poke (p `plusPtr` idx) w
     let buf' = RingBuffer fp $ (idx + 1) .&. 32767
     return $ case buf' of
         RingBuffer _ 0 -> S.copy (fromForeignPtr fp 0 32768) `chunk` k buf'
         _ -> k buf'
-copy :: Int -> SlidingWindow ()
+copy :: Int -> SlidingWindow
 copy dist = SlidingWindow $ \ k buf@(RingBuffer fp idx) -> inlinePerformIO $ do
         w <- withForeignPtr fp $ \ p -> peek (p `plusPtr` ((idx - dist) .&. 32767))
         let (SlidingWindow k') = write w
         return $ k' k buf
-extract :: SlidingWindow () -> L.ByteString
+extract :: SlidingWindow -> L.ByteString
 extract (SlidingWindow k) = k finish newRing where
     newRing = RingBuffer (unsafePerformIO $ mallocByteString 32768) 0
     finish (RingBuffer fp len) = (fromForeignPtr fp 0 len) `chunk` L.empty
 
 inflateBlocks :: [DeflateBlock] -> L.ByteString
-inflateBlocks = extract . mapM_ doBlock where
-    doBlock (Uncompressed bs) = mapM_ write $ S.unpack bs
-    doBlock (LempelZiv lzs) = mapM_ unLZ lzs
-    unLZ (LZRef len dist) = replicateM_ len $ copy dist
+inflateBlocks = extract . Foldable.foldMap doBlock where
+    doBlock (Uncompressed bs) = Foldable.foldMap write $ S.unpack bs
+    doBlock (LempelZiv lzs) = Foldable.foldMap unLZ lzs
+    unLZ (LZRef len dist) = mconcat $ replicate len $ copy dist
     unLZ (LZLit c) = write c
-    unLZ LZEOF = return ()
+    unLZ LZEOF = mempty
 
 unpredict :: [DeflateBlock] -> [[(Int, Int)]] -> [(Int, Int)]
 unpredict [] [] = []
